1e8a86d105d67e2036fc49df4fd7da6de483dfc6
RockOnJeet <soumya2004jit@gmail.com>
2024-08-01 14:12:17 +0530

    Checked Encoder Functions using Preliminary Codes

--- Omni-Bot.ino
+++ Omni-Bot.ino
@@ -1,115 +1,47 @@
-// #include <SoftwareSerial.h>
-
-// SoftwareSerial Serial(10, 11); // RX, TX pins for Bluetooth module
-
-// Define the PWM and DIR pins for each motor
-
-const int pwmPin1 = 9;  // Replace with the actual PWM pin for motor 1
-const int dirPin1 = 5;  // Replace with the actual DIR pin for motor 1
-
-const int pwmPin2 = 10;  // Replace with the actual PWM pin for motor 2
-const int dirPin2 = 6;   // Replace with the actual DIR pin for motor 2
-
-const int pwmPin3 = 11;  // Replace with the actual PWM pin for motor 3
-const int dirPin3 = 7;   // Replace with the actual DIR pin for motor 3
+volatile int temp,
+    counter = 0;  // This variable will increase or decrease depending on the
+                  // rotation of encoder
 
 void setup() {
-  // Serial.begin(9600);
   Serial.begin(9600);
-  pinMode(pwmPin1, OUTPUT);
-  pinMode(dirPin1, OUTPUT);
-  pinMode(pwmPin2, OUTPUT);
-  pinMode(dirPin2, OUTPUT);
-  pinMode(pwmPin3, OUTPUT);
-  pinMode(dirPin3, OUTPUT);
+
+  pinMode(2, INPUT_PULLUP);  // internal pullup input pin 2
+
+  pinMode(3, INPUT_PULLUP);  // internalเป็น pullup input pin 3
+                             // Setting up interrupt
+  // A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is
+  // DigitalPin nr 2 on moust Arduino.
+  attachInterrupt(0, ai0, RISING);
+
+  // B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is
+  // DigitalPin nr 3 on moust Arduino.
+  attachInterrupt(1, ai1, RISING);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char command = Serial.read();
-    // Control commands from Bluetooth
-    switch (command) {
-      case 'F':  // Forward
-        Serial.print(command);
-        digitalWrite(dirPin1, HIGH);
-        analogWrite(pwmPin1, 0);
-        digitalWrite(dirPin2, LOW);
-        analogWrite(pwmPin2, 255);
-        digitalWrite(dirPin3, HIGH);
-        analogWrite(pwmPin3, 255);
-        break;
-      case 'B':  // Backward
-        digitalWrite(dirPin1, HIGH);
-        analogWrite(pwmPin1, 0);
-        digitalWrite(dirPin2, HIGH);
-        analogWrite(pwmPin2, 225);
-        digitalWrite(dirPin3, LOW);
-        analogWrite(pwmPin3, 225);
-        Serial.print(command);
-        break;
-      case 'L':  // Left
-        Serial.print(command);
-        digitalWrite(dirPin1, HIGH);
-        analogWrite(pwmPin1, 254);
-        digitalWrite(dirPin2, LOW);
-        analogWrite(pwmPin2, 126);
-        digitalWrite(dirPin3, LOW);
-        analogWrite(pwmPin3, 126);
-        break;
-      case 'R':
-        digitalWrite(dirPin1, LOW);
-        analogWrite(pwmPin1, 254);
-        digitalWrite(dirPin2, HIGH);
-        analogWrite(pwmPin2, 126);
-        digitalWrite(dirPin3, HIGH);
-        analogWrite(pwmPin3, 126);
-        Serial.print(command);
-        break;
-      case 'G':
-        Serial.print(command);
-        digitalWrite(dirPin1, HIGH);
-        analogWrite(pwmPin1, 255);
-        digitalWrite(dirPin2, LOW);
-        analogWrite(pwmPin2, 255);
-        digitalWrite(dirPin3, HIGH);
-        analogWrite(pwmPin3, 127.5);
-        break;
-      case 'H':
-        digitalWrite(dirPin1, HIGH);
-        analogWrite(pwmPin1, 255);
-        digitalWrite(dirPin2, HIGH);
-        analogWrite(pwmPin2, 127.5);
-        digitalWrite(dirPin3, LOW);
-        analogWrite(pwmPin3, 255);
-        Serial.print(command);
-        break;
-      case 'I':
-        digitalWrite(dirPin1, LOW);
-        analogWrite(pwmPin1, 255);
-        digitalWrite(dirPin2, LOW);
-        analogWrite(pwmPin2, 127.5);
-        digitalWrite(dirPin3, HIGH);
-        analogWrite(pwmPin3, 255);
-        Serial.print(command);
-        break;
-      case 'J':
-        digitalWrite(dirPin1, LOW);
-        analogWrite(pwmPin1, 255);
-        digitalWrite(dirPin2, HIGH);
-        analogWrite(pwmPin2, 255);
-        digitalWrite(dirPin3, LOW);
-        analogWrite(pwmPin3, 127.5);
-        Serial.print(command);
-        break;
-      case 'S':  // Stop
-        Serial.print(command);
-        digitalWrite(dirPin1, HIGH);
-        analogWrite(pwmPin1, 0);
-        digitalWrite(dirPin2, LOW);
-        analogWrite(pwmPin2, 0);
-        digitalWrite(dirPin3, LOW);
-        analogWrite(pwmPin3, 0);
-        break;
-    }
+  // Send the value of counter
+  if (counter != temp) {
+    Serial.println(counter);
+    temp = counter;
+  }
+}
+
+void ai0() {
+  // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
+  // Check pin 3 to determine the direction
+  if (digitalRead(3) == LOW) {
+    counter++;
+  } else {
+    // counter--;
+  }
+}
+
+void ai1() {
+  // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
+  // Check with pin 2 to determine the direction
+  if (digitalRead(2) == LOW) {
+    counter--;
+  } else {
+    // counter++;
   }
 }
