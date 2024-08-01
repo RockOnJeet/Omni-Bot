d8aa59694e2ddeed38370cdcb25fdc7ac7518fac
RockOnJeet <soumya2004jit@gmail.com>
2024-08-01 14:29:45 +0530

    Major Rewrite of Encoder Code, without using Interrupts

--- Omni-Bot.ino
+++ Omni-Bot.ino
@@ -1,47 +1,47 @@
-volatile int temp,
-    counter = 0;  // This variable will increase or decrease depending on the
-                  // rotation of encoder
+#include <MD_REncoder.h>
+
+volatile int xCounter, yCounter, X, Y;
+
+MD_REncoder encoder[2] = {MD_REncoder(2, 3), MD_REncoder(19, 18)};
 
 void setup() {
-  Serial.begin(9600);
-
-  pinMode(2, INPUT_PULLUP);  // internal pullup input pin 2
-
-  pinMode(3, INPUT_PULLUP);  // internalเป็น pullup input pin 3
-                             // Setting up interrupt
-  // A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is
-  // DigitalPin nr 2 on moust Arduino.
-  attachInterrupt(0, ai0, RISING);
-
-  // B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is
-  // DigitalPin nr 3 on moust Arduino.
-  attachInterrupt(1, ai1, RISING);
+  Serial.begin(115200);
+  encoder[0].begin();
+  encoder[1].begin();
+  Serial.println("Ready");
 }
 
 void loop() {
-  // Send the value of counter
-  if (counter != temp) {
-    Serial.println(counter);
-    temp = counter;
-  }
+  getEncodeData();
 }
 
-void ai0() {
-  // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
-  // Check pin 3 to determine the direction
-  if (digitalRead(3) == LOW) {
-    counter++;
-  } else {
-    // counter--;
+void getEncodeData() {
+  byte x = encoder[0].read();
+  byte y = encoder[1].read();
+  if (x != DIR_NONE) {
+    // Serial.print(x == DIR_CW ? "\n+1x" : "\n-1x");
+    xCounter += x == DIR_CW ? 1 : -1;
+    if (xCounter > 400 || xCounter < -400) {
+      if (xCounter > 400)
+        X++;
+      else
+        X--;
+      Serial.print("XRevs: ");
+      Serial.println(X);
+      xCounter = 0;
+    }
   }
-}
-
-void ai1() {
-  // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
-  // Check with pin 2 to determine the direction
-  if (digitalRead(2) == LOW) {
-    counter--;
-  } else {
-    // counter++;
+  if (y != DIR_NONE) {
+    // Serial.print(y == DIR_CW ? "\n+1y" : "\n-1y");
+    yCounter += y == DIR_CW ? 1 : -1;
+    if (yCounter > 400 || yCounter < -400) {
+      if (yCounter > 400)
+        Y++;
+      else
+        Y--;
+      Serial.print("YRevs: ");
+      Serial.println(Y);
+      yCounter = 0;
+    }
   }
 }
