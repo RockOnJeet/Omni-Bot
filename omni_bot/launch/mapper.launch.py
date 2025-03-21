90868257f6eaab849df0af543bd10eedd9e11515
LOQ-Ubuntu <soumya2004jit@outlook.com>
2025-03-21 21:30:41 +0530

    Added mapping stuff

---
+++ omni_bot/launch/mapper.launch.py
@@ -0,0 +1,92 @@
+import os
+import xacro
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch_ros.actions import Node
+
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
+from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.event_handlers import OnProcessExit
+
+from launch.conditions import IfCondition
+
+
+def generate_launch_description():
+    path = get_package_share_directory('omni_bot')
+
+    # Arguments
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    time_arg = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='false',
+        description='Use simulation/Gazebo clock'
+    )
+
+    use_rviz = LaunchConfiguration('use_rviz')
+    rviz_arg = DeclareLaunchArgument(
+        name='use_rviz',
+        default_value='true',
+        description='Whether to start rviz'
+    )
+
+    # Nodes
+    rviz_config = os.path.join(
+        path,
+        'config',
+        'rviz',
+        'map_view.rviz'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+
+        output='screen',
+        arguments=['-d', rviz_config],
+        condition=IfCondition(use_rviz)
+    )
+
+    bot_xacro = os.path.join(
+        path,
+        'description',
+        'omni_bot.urdf.xacro'
+    )
+    robot_description = xacro.process_file(bot_xacro).toxml()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description,
+                     'use_sim_time': use_sim_time}]
+    )
+
+    # Run mapping node
+    mapper_params = {
+        'use_sim_time': use_sim_time
+    }
+    mapper_launch = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+        os.path.join(path, 'launch', 'online_async_launch.py')
+      ),
+      launch_arguments=mapper_params.items()
+    )
+    
+
+    # Launch!
+    return LaunchDescription([
+        # Arguments
+        time_arg,
+        rviz_arg,
+
+        # Nodes
+        robot_state_publisher_node,
+        rviz_node,
+        
+        # Launch Files
+        # mapper_launch
+    ])
