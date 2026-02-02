0d0cdb19d91d926dcd5ce909c3708169c72965c2
LOQ-Ubuntu <soumyajit@vssut.ac.in>
2026-02-03 02:37:10 +0530

    Minor, but affecting: - Added `use_sim_time` arg with Logging.

--- omni_bot_bringup/launch/bringup.launch.py
+++ omni_bot_bringup/launch/bringup.launch.py
@@ -1,8 +1,25 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, LogInfo
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
+    """Launch file for Omni Bot kinematics and odometry nodes."""
+
+    # Arguments
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    time_arg = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='false',
+        description='Use simulation/Gazebo clock'
+    )
+
+    # Log sim time setting
+    sim_time_info = LogInfo(
+        msg=['NOTE: Sim time set to ', use_sim_time]
+    )
+
     # Kinematics node (cmd_vel -> wheel velocities)
     kine_node = Node(
         package='omni_bot_kinematics',
@@ -12,7 +29,7 @@ def generate_launch_description():
         parameters=[{
             'wheel_radius': 0.076,
             'robot_radius': 0.4185,
-            'use_sim_time': False
+            'use_sim_time': use_sim_time
         }]
     )
 
@@ -25,11 +42,13 @@ def generate_launch_description():
         parameters=[{
             'encoder_radius': 0.05,
             'update_rate': 60.0,
-            'use_sim_time': False
+            'use_sim_time': use_sim_time
         }]
     )
 
     return LaunchDescription([
+        time_arg,
+        sim_time_info,
         kine_node,
         odometry_node
     ])
